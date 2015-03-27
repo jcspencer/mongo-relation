f7e31d20f259b5be25bbf829279e9bb4c62ab134
Jonathon Storer <jonathon.scott.storer@gmail.com>
2015-03-27 13:32:05 -0400

    remove hasOne relationship
    closes #18

--- specs/support/twitterPostModel.js
+++ specs/support/twitterPostModel.js
@@ -4,7 +4,6 @@ var twitterPostSchema = new mongoose.Schema({
   title: String
 });
 
-twitterPostSchema.belongsTo('editor', { modelName: 'TwitterUser' });
 twitterPostSchema.belongsTo('author', { modelName: 'TwitterUser' });
 
 // should not delete the reference
--- specs/support/twitterUserModel.js
+++ specs/support/twitterUserModel.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 
 var twitterUserSchema = new mongoose.Schema({ name: String });
 
-twitterUserSchema.hasOne('post', { modelName: 'TwitterPost' });
-
 twitterUserSchema.hasMany('categories');
 twitterUserSchema.hasMany('tags', { dependent: 'nullify' });
 twitterUserSchema.hasMany('tweets', { dependent: 'delete', inverse_of: 'author' });
